bb09a17096a7d1f02d1594f3c1e7b036ac18749d
Administrator <Administrator@KDTKBCVERCBKIHZ>
2018-04-20 11:51:02 +0800

    我的第二次测试

---
+++ ssm_test-2/src/main/java/com/hr/SsmTest2Application.java
@@ -0,0 +1,12 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmTest2Application.class, args);
+	}
+}
